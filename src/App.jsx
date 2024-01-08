217b28be464e2762fa1af5b9b3cf628f68c4c049
Joey Parrish <joey.parrish@gmail.com>
2024-01-08 13:01:15 -0800

    Split out help modal content into component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Input from "./Input";
 import Box from "./Box";
 import Buttons from "./Buttons";
+import Help from "./Help";
 import Modal from "./Modal";
 import Won from "./Won";
 import { GameProvider } from './context';
-import reference from './assets/reference.svg';
 
 export default function App() {
   return (
@@ -19,24 +19,7 @@ export default function App() {
         <Buttons />
       </div>
       <Modal id="helpModal" stateName="help">
-        <h1>How To Play</h1>
-
-        <p>Create words using letters around the square</p>
-
-        <img src={reference} alt="example game" />
-
-        <ul>
-          <li>Connect letters to spell words</li>
-          <li>Words must be at least 3 letters long</li>
-          <li>Consecutive letters cannot be from the same side</li>
-          <li>The last letter of a word becomes the first letter of the next word (e.g. THY &gt; YES &gt; SINCE)</li>
-          <li>Words cannot be proper nouns or hyphenated</li>
-          <li>No cussing, either (sorry)</li>
-          <li>Use all letters to win!</li>
-        </ul>
-
-        <p>New puzzles are released at 3 a.m. EST.</p>
-        <p>The original game can be found <a href="https://www.nytimes.com/puzzles/letter-boxed">here</a>.</p>
+        <Help />
       </Modal>
       <Modal id="wonModal" stateName="won">
         <Won />
---
+++ src/Help/index.jsx
@@ -0,0 +1,26 @@
+import reference from '../assets/reference.svg';
+
+export default function Help() {
+  return (
+    <div className="fill">
+      <h1>How To Play</h1>
+
+      <p>Create words using letters around the square</p>
+
+      <img src={reference} alt="example game" />
+
+      <ul>
+        <li>Connect letters to spell words</li>
+        <li>Words must be at least 3 letters long</li>
+        <li>Consecutive letters cannot be from the same side</li>
+        <li>The last letter of a word becomes the first letter of the next word (e.g. THY &gt; YES &gt; SINCE)</li>
+        <li>Words cannot be proper nouns or hyphenated</li>
+        <li>No cussing, either (sorry)</li>
+        <li>Use all letters to win!</li>
+      </ul>
+
+      <p>New puzzles are released at 3 a.m. EST.</p>
+      <p>The original game can be found <a href="https://www.nytimes.com/puzzles/letter-boxed">here</a>.</p>
+    </div>
+  );
+}
