12b1d33969e6252120c14bf01c0d8b7cb2aa9a83
Joey Parrish <joey.parrish@gmail.com>
2024-01-19 14:30:53 -0800

    Add service worker for offline usage

---
+++ build-service-worker.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+(async () => {
+  const {generateSW} = require('workbox-build');
+  const {count, size, warnings} = await generateSW({
+    globDirectory: 'dist/',
+    globPatterns: ['!(*.map)'],
+    skipWaiting: true,
+    swDest: 'dist/service-worker.js',
+  });
+
+  if (warnings.length > 0) {
+    console.warn(`Warnings encountered while generating a service worker: ${warnings.join('\n')}`);
+  }
+  console.log(`Generated a service worker to precache ${count} files, totaling ${size} bytes.`);
+})();
--- src/main.jsx
+++ src/main.jsx
@@ -6,3 +6,19 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('container')).render(
   <App />
 );
+
+(async () => {
+  if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    try {
+      const registration =
+          await navigator.serviceWorker.register('service-worker.js');
+      console.log('Service worker registration succeeded:', registration);
+    } catch (error) {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  } else {
+    console.error('Service workers are not supported.');
+  }
+})();
