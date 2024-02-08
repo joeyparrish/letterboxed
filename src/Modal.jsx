15c89bd1f729ddef71a864f45483a276d98b1392
Joey Parrish <joey.parrish@gmail.com>
2024-02-08 14:07:35 -0800

    Improve modal display, add scrim
    Closes #11

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,13 +1,16 @@
 export default function Modal({ id, open, onClose, children }) {
-  const className = open ? 'modal open' : 'modal';
   if (open) {
     document.activeElement.blur();
+    return (
+      <>
+        <div className="modalScrim"></div>
+        <div id={id} className="modal">
+          <button className="closeButton" onClick={onClose}>&times;</button>
+          { children }
+        </div>
+      </>
+    );
+  } else {
+    return (<></>);
   }
-
-  return (
-    <div id={id} className={className}>
-      <button className="closeButton" onClick={onClose}>&times;</button>
-      { children }
-    </div>
-  );
 }
